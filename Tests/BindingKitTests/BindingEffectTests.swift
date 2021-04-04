f0f7bec0b79f951e5bd597db1c7601080d6bc621
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2021-04-04 14:43:59 +0200

    Add binding side-effect tests

---
+++ Tests/BindingKitTests/BindingEffectTests.swift
@@ -0,0 +1,34 @@
+import XCTest
+import SwiftUI
+import BindingKit
+
+final class BindingEffectTests: XCTestCase {
+    func testWillSet() {
+        var innerValue = "firstValue"
+        let binding = Binding { innerValue } set: { innerValue = $0 }
+        
+        let expectation = self.expectation(description: "Will set called")
+        let bindingWithEffect = binding.willSet { newValue in
+            XCTAssertEqual(binding.wrappedValue, "firstValue")
+            XCTAssertEqual(newValue, "secondValue")
+            expectation.fulfill()
+        }
+        bindingWithEffect.wrappedValue = "secondValue"
+        wait(for: [expectation], timeout: 0)
+    }
+    
+    func testDidSet() {
+        var innerValue = "firstValue"
+        let binding = Binding { innerValue } set: { innerValue = $0 }
+        
+        let expectation = self.expectation(description: "Will set called")
+        let bindingWithEffect = binding.didSet { newValue in
+            XCTAssertEqual(binding.wrappedValue, "secondValue")
+            XCTAssertEqual(newValue, "secondValue")
+            expectation.fulfill()
+        }
+        bindingWithEffect.wrappedValue = "secondValue"
+        wait(for: [expectation], timeout: 0)
+    }
+}
+
