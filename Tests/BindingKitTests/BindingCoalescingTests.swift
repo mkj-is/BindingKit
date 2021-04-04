5e57d14266b702c1d81d06c922442c33f623649e
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2021-04-04 14:40:47 +0200

    Add coalescing tests

--- Tests/BindingKitTests/BindingCoalescingTests.swift
+++ Tests/BindingKitTests/BindingCoalescingTests.swift
@@ -1,8 +1,35 @@
 import XCTest
+import SwiftUI
 @testable import BindingKit
 
-final class BindingKitTests: XCTestCase {
-    func testExample() {
-        XCTAssertTrue(true)
+final class BindingCoalescingTests: XCTestCase {
+    func testBasicCoalescing() {
+        let binding = Binding<String?>.constant(nil) ?? "fallback"
+        XCTAssertEqual(binding.wrappedValue, "fallback")
+    }
+    
+    func testBasicCoalescingWithValue() {
+        let binding = Binding<String?>.constant("value") ?? "fallback"
+        XCTAssertEqual(binding.wrappedValue, "value")
+    }
+    
+    func testOptionalCoalescingWithNil() {
+        let binding = Binding<String?>.constant(nil) ?? nil
+        XCTAssertEqual(binding.wrappedValue, nil)
+    }
+    
+    func testOptionalCoalescingWithValue() {
+        let binding: Binding<String?> = .constant(nil) ?? "value"
+        XCTAssertEqual(binding.wrappedValue, "value")
+    }
+    
+    func testOptionalCoalescingWithConstantValue() {
+        let binding: Binding<String?> = .constant("value") ?? nil
+        XCTAssertEqual(binding.wrappedValue, "value")
+    }
+    
+    func testCoalescingChain() {
+        let binding = Binding<String?>.constant(nil) ?? nil ?? "fallback"
+        XCTAssertEqual(binding.wrappedValue, "fallback")
     }
 }
